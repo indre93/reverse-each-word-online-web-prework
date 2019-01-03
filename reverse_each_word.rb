f7af3141cfada532a71624c5faf80f8bdd95d768
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-03 05:45:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence = "Hello there, and how are you?")
-  sentence.split
-  sentence.collect do |word|
+  array = sentence.split
+  array.collect do |word|
     word.reverse!
   end
 end
